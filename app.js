49176c1f3bbd34dfb1fe49d102ff777770f4059d
Magnus Skog <magnus.m.skog@gmail.com>
2019-01-19 11:42:14 +0100

    Added metascraper thing

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const metascraper = require("metascraper")([
+  require("metascraper-author")(),
+  require("metascraper-date")(),
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-clearbit-logo")(),
+  require("metascraper-publisher")(),
+  require("metascraper-title")(),
+  require("metascraper-url")()
+]);
+
+const got = require("got");
+
+app.get("/", (req, res) => {
+  const targetUrl = req.query["url"];
+
+  (async () => {
+    const { body: html, url } = await got(targetUrl);
+    const metadata = await metascraper({ html, url });
+    res.send(metadata);
+  })();
+});
